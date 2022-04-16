e23d0654d8cc38a520bb8eda65f68750972be691
MAXIM <victor-kovalov@yandex.ru>
2022-04-16 23:18:10 +0300

    Update CRUD

--- internal/server/server.go
+++ internal/server/server.go
@@ -12,34 +12,34 @@ type CRUDServer struct {
 	DbConn *sql.DB
 }
 
-func (c *CRUDServer) CreateNote(ctx context.Context, r *crud.Request) (*crud.Response, error) {
+func (c *CRUDServer) CreateNote(ctx context.Context, r *crud.Request) (*crud.Status, error) {
 	_, err := c.DbConn.Exec("insert into blog (title, text, tags) values ($1, $2, $3)", r.Title, r.Text, r.Tags)
 	if err != nil {
-		return &crud.Response{Status: "500 Internal error"}, err
+		return &crud.Status{Status: "500 Internal error"}, err
 	}
-	return &crud.Response{Status: "200 Ok"}, nil
+	return &crud.Status{Status: "200 Ok"}, nil
 }
 
 func (c *CRUDServer) ReadNote(ctx context.Context, r *crud.Request) (*crud.Response, error) {
 	resp := crud.Response{}
-	rows := c.DbConn.QueryRow("select title, text, tags, created_at, updated_at from blog where id = $1", r.Id)
-	rows.Scan(&resp.Title, &resp.Text, &resp.Tags, &resp.CreatedAt, &resp.UpdatedAt)
+	rows := c.DbConn.QueryRow("select id, title, text, tags from blog where id = $1", r.Id)
+	rows.Scan(&resp.Id, &resp.Title, &resp.Text, &resp.Tags)
 	resp.Status = "200 Ok"
 	return &resp, nil
 }
 
-func (c *CRUDServer) UpdateNote(ctx context.Context, r *crud.Request) (*crud.Response, error) {
-	_, err := c.DbConn.Exec("update blog set title = $1, text = $2, tags = $3, updated_at = $4 where id = $5", r.Title, r.Text, r.Tags, r.UpdatedAt, r.Id)
+func (c *CRUDServer) UpdateNote(ctx context.Context, r *crud.Request) (*crud.Status, error) {
+	_, err := c.DbConn.Exec("update blog set title = $1, text = $2, tags = $3 where id = $4", r.Title, r.Text, r.Tags, r.Id)
 	if err != nil {
-		return &crud.Response{Status: "500 Internal error"}, err
+		return &crud.Status{Status: "500 Internal error"}, err
 	}
-	return &crud.Response{Status: "200 Ok"}, nil
+	return &crud.Status{Status: "200 Ok"}, nil
 }
 
-func (c *CRUDServer) DeleteNote(ctx context.Context, r *crud.Request) (*crud.Response, error) {
-	_, err := c.DbConn.Exec("delete from dlog where id = $1", r.Id)
+func (c *CRUDServer) DeleteNote(ctx context.Context, r *crud.Request) (*crud.Status, error) {
+	_, err := c.DbConn.Exec("delete from blog where id = $1", r.Id)
 	if err != nil {
-		return &crud.Response{Status: "500 Internal error"}, err
+		return &crud.Status{Status: "500 Internal error"}, err
 	}
-	return &crud.Response{Status: "200 Ok"}, nil
+	return &crud.Status{Status: "200 Ok"}, nil
 }
