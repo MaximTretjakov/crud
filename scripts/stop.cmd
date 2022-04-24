ea9e0b3880bf888a04edb51348055b4306a74b1b
Maxim Tretyakov <victor-kovalov@yandex.ru>
2022-04-24 16:35:34 +0300

    Support Linux

--- scripts/stop.cmd
+++
@@ -1,2 +0,0 @@
-echo "Docker shoutdown..."
-docker-compose -f ../build/docker-compose.yml down
---
+++ scripts/stop.sh
@@ -0,0 +1,2 @@
+echo "Docker shoutdown..."
+docker-compose --compatibility -f ../build/docker-compose.yml down
